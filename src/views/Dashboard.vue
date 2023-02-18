10535b9096369cae9a03f1772a022fd8bbddba6e
K. Canamar | Remote Full Stack Software Engineer <99228411+kcanamar@users.noreply.github.com>
2023-02-18 13:21:17 -0800

    dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  setup(){
+     const logout = () => {
+     console.log("User has been logged out")
+    };
+    return {
+      logout,
+    };
+  }
+}
+</script>
+
+<template>
+    <div class="dashboard-container">
+        <h2>Welcome to our Dashboard Page</h2>
+        <p>Hello!</p>
+        <button @click="logout">Sign out</button>
+    </div>
+</template>
+
+<style scoped>
+</style>
